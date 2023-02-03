e2d41f4d0c350e7a36501c641b385a1aa42d8fba
Madison Evans <m.corbinevans@gmail.com>
2023-02-03 15:58:35 -0700

    installed react router and added nav

---
+++ src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+/**
+ * Navigation component that displays the navigation links of the app
+ * @function
+ * @returns {JSX.Element}
+ */
+const Navigation = () => {
+	return (
+		<NavContainer>
+			<NavLink to="/">Home</NavLink>
+			<NavLink to="/about">About</NavLink>
+			<NavLink to="/services">Services</NavLink>
+			<NavLink to="/contact">Contact</NavLink>
+		</NavContainer>
+	);
+};
+
+/**
+ * Styled component for the navigation container
+ */
+const NavContainer = styled.nav`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem;
+	background-color: #333;
+	color: #fff;
+`;
+
+/**
+ * Styled component for the navigation links
+ */
+const NavLink = styled(Link)`
+	color: #fff;
+	text-decoration: none;
+	margin-right: 1rem;
+	&:hover {
+		color: #aaa;
+	}
+`;
+
+export default Navigation;
