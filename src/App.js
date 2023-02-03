d48879376e0d03bcb6f293ad6178c0768dbf0965
Madison Evans <m.corbinevans@gmail.com>
2023-02-03 15:56:03 -0700

    footer added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import ContactForm from "./Components/Form/ContactForm";
+import Footer from "./Components/Footer/Footer";
 
 /**
  * //[ ]Header component: A component that displays the header of the app, usually containing the logo and navigation links.
 
 //[ ] Navigation component: A component that displays the navigation menu, allowing users to navigate between different pages or sections of the app.
 
-//[ ] Footer component: A component that displays the footer of the app, usually containing copyright information, links to the company's social media profiles, and other relevant information.
+//[x] Footer component: A component that displays the footer of the app, usually containing copyright information, links to the company's social media profiles, and other relevant information.
 
 //[ ] Hero component: A component that displays a large banner image or video, with text or other elements overlaid on top, usually used to promote the app's main features or values.
 
 //[ ] Card component: A component that displays a compact representation of an item or content, usually used to display a list of items in a grid or other layout.
 
 //[x] Form component: A component that displays a form for users to input data, typically used for user registration, login, or data submission.
-
 
 //[ ] Modal component: A component that displays a pop-up dialog, typically used to display additional information or to prompt the user for input.
 
@@ -26,6 +26,7 @@ function App() {
 	return (
 		<>
 			<ContactForm />
+			<Footer />
 		</>
 	);
 }
