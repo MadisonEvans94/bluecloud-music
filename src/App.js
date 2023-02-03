5f2e461557d3ed7f6f681f60e185c46a9102ad96
Madison Evans <m.corbinevans@gmail.com>
2023-02-03 15:07:44 -0700

    checkpoint

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ function App() {
 		<>
 			{/* Message form  */}
 			<div className="form-container">
-				<h2>Form</h2>
+				<h2>Contact Form</h2>
 				<form>
 					{/* name */}
 					<label htmlFor="name-input">Name</label>
 					<input type="text" required id="name-input" />
 					{/* email */}
-					<label htmlFor="email-input">Name</label>
+					<label htmlFor="email-input">Email</label>
 					<input type="text" required id="email-input" />
 					{/* message type (radio buttons) */}
-					<label htmlFor="message-type">Name</label>
+					<label htmlFor="message-type">Reason for Contact</label>
 					<input type="text" required id="message-type" />
 					{/* text area */}
-					<label htmlFor="text-area">Name</label>
+					<label htmlFor="text-area">Message</label>
 					<textarea name="" id="text-area" cols="30" rows="10"></textarea>
 				</form>
 			</div>
