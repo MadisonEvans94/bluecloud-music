2e1a16521adc142a1cbdabe1714f57866c476a94
Madison Evans <m.corbinevans@gmail.com>
2023-02-04 11:30:40 -0700

    created routes for pages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
+import Music from "./Pages/Music/Music";
+import Checkout from "./Pages/Checkout/Checkout";
+import Contact from "./Pages/Contact/Contact";
 import PageNotFound from "./Pages/PageNotFound/PageNotFound";
 
 function App() {
 	return (
 		<Routes>
 			<Route exact path="/" element={<Home />} />
+			<Route path="/music" element={<Music />} />
+			<Route path="/checkout" element={<Checkout />} />
+			<Route path="/contact" element={<Contact />} />
 
 			{/* this will be our 404 redirect page */}
-			<Route exact path="*" element={<PageNotFound />} />
+			<Route path="*" element={<PageNotFound />} />
 		</Routes>
 	);
 }
