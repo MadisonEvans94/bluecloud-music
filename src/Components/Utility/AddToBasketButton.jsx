8412b6391204be13a4a9595a0fc0542fc95d171d
Madison Evans <m.corbinevans@gmail.com>
2023-02-04 14:22:44 -0700

    add to basket button plugged in

--- src/Components/Utility/AddToBasketButton.jsx
+++ src/Components/Utility/AddToBasketButton.jsx
@@ -28,19 +28,39 @@ const Button = styled.div`
 	padding: 0.25rem;
 	cursor: pointer;
 `;
+const SubmitButton = styled.div`
+	padding: 1rem;
+	background-color: rgba(255, 255, 255, 0.5);
+	transition: 0.2s;
+	cursor: pointer;
+	&:hover {
+		border-radius: 15px;
+	}
+`;
 
+/**
+ * AddToBasketButton component
+ * @returns {JSX.Element} The rendered component
+ */
 const AddToBasketButton = () => {
 	const [qty, setQty] = useState(0);
 	const handleIncrease = () => setQty(qty + 1);
 	const handleDecrease = () => (qty > 0 ? setQty(qty - 1) : null);
+	const addToBasket = () => {
+		console.log(
+			`in future iterations, we will be sending qty value '${qty}' to the basket context`
+		);
+		setQty(0);
+	};
 	return (
 		<Container>
-			<Title>Add to Basket</Title>
+			<Title>QTY</Title>
 			<Controls>
 				<Button onClick={handleDecrease}>-</Button>
 				<div>{qty}</div>
 				<Button onClick={handleIncrease}>+</Button>
 			</Controls>
+			<SubmitButton onClick={addToBasket}>Add to Basket</SubmitButton>
 		</Container>
 	);
 };
