3016b0915c0ca9f61aa36d9886c94fadbe9f98e0
Madison Evans <m.corbinevans@gmail.com>
2023-02-04 17:24:27 -0700

    added framer and rearranged Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation";
+import { BrowserRouter } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
 import Music from "./Pages/Music/Music";
@@ -14,27 +15,29 @@ const backgroundColor = "#2ca7ff";
 function App() {
 	return (
 		<>
-			<div
-				style={{
-					backgroundColor: `${backgroundColor}`,
-					position: "absolute",
-					width: "100%",
-					height: "100%",
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}>
-				<Navigation />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/music" element={<Music />} />
-					<Route path="/checkout" element={<Checkout />} />
-					<Route path="/contact" element={<Contact />} />
+			<BrowserRouter>
+				<div
+					style={{
+						backgroundColor: `${backgroundColor}`,
+						position: "absolute",
+						width: "100%",
+						height: "100%",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}>
+					<Navigation />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/music" element={<Music />} />
+						<Route path="/checkout" element={<Checkout />} />
+						<Route path="/contact" element={<Contact />} />
 
-					{/* this will be our 404 redirect page */}
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</div>
+						{/* this will be our 404 redirect page */}
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
 		</>
 	);
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,18 +2,9 @@ import React from "react";
 import ProductCard from "../../Components/Card/ProductCard";
 
 const Home = () => {
-	/**
-	 * A card component that displays a background image, title, description, and price
-	 *
-	 * @param {string} backgroundImg - The URL of the background image
-	 * @param {string} title - The title to be displayed
-	 * @param {string} description - The description to be displayed
-	 * @param {string} price - The price to be displayed
-	 */
-
 	const backgroundImg =
 		"https://marketplace.canva.com/EAFJ9TROiw8/1/0/1600w/canva-renaissance-bust-vaporwave-aesthetic-album-cover-SVO6_i94IGk.jpg";
-	const title = "Parthanon";
+	const title = "Blue Tape Vol: 1";
 	const description =
 		"Introducing a new music sample pack for producers and beat-makers. This pack offers a diverse collection of high-quality samples, including drums, synths, basslines, and more. Suitable for various genres, elevate your productions with this must-have pack. Get your copy now!";
 	const price = "$30";
