5b02b24b51cd36e372e46d854f33607675efd7c2
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    added animation back to mymusic container

--- src/Components/MyMusic/MyMusic.jsx
+++ src/Components/MyMusic/MyMusic.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import CatalogItem from "../CatalogItem/CatalogItem";
 import img from "../../assets/images/songImg.png";
 import CatalogContainer from "../CatalogContainer/CatalogContainer";
-
+import { motion } from "framer-motion";
 const MyMusic = () => {
 	return (
-		<div className="my-music-container">
+		<motion.div
+			className="my-music-container"
+			initial={{ x: 600, opacity: 0 }}
+			animate={{ x: 0, opacity: 1 }}
+			transition={{ type: "spring", stiffness: 100, damping: 20 }}>
 			<h2 className="my-music-header">My Music</h2>
 			<CatalogContainer>
 				<CatalogItem img={img} />
 				<CatalogItem img={img} />
 				<CatalogItem img={img} />
 			</CatalogContainer>
-		</div>
+		</motion.div>
 	);
 };
 
