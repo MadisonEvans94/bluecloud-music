5830a943df7d2f85b1ffcc98ef88981d270fec3c
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    minor changes

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProductCard from "../../Components/Card/ProductCard";
 
+const background =
+	"https://marketplace.canva.com/EAFJ9TROiw8/1/0/1600w/canva-renaissance-bust-vaporwave-aesthetic-album-cover-SVO6_i94IGk.jpg";
 const ProductPage = () => {
-	const backgroundImg =
-		"https://marketplace.canva.com/EAFJ9TROiw8/1/0/1600w/canva-renaissance-bust-vaporwave-aesthetic-album-cover-SVO6_i94IGk.jpg";
+	//assets/images/honeypack.png";
+
 	const title = "Blue Tape Vol: 1";
 	const description =
 		"Introducing a new music sample pack for producers and beat-makers. This pack offers a diverse collection of high-quality samples, including drums, synths, basslines, and more. Suitable for various genres, elevate your productions with this must-have pack. Get your copy now!";
@@ -14,7 +16,7 @@ const ProductPage = () => {
 			<ProductCard
 				title={title}
 				description={description}
-				backgroundImg={backgroundImg}
+				backgroundImg={background}
 				price={price}
 			/>
 			;
