dacd76d61e6f874f84809c732d7e2e0b0bf52009
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    music links animated

--- src/Components/MusicHero/MusicHero.jsx
+++ src/Components/MusicHero/MusicHero.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const MusicHero = ({ icon }) => {
 	return (
 		<div className="music-link-container">
-			<a href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
+			<motion.a
+				initial={{ scale: 1 }}
+				whileHover={{ scale: 1.25 }}
+				href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
 				<img className="music-link-icon" src={icon} alt="logo" />
-			</a>
-			<a href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
+			</motion.a>
+			<motion.a
+				initial={{ scale: 1 }}
+				whileHover={{ scale: 1.25 }}
+				href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
 				<img className="music-link-icon" src={icon} alt="logo" />
-			</a>
-			<a href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
+			</motion.a>
+			<motion.a
+				initial={{ scale: 1 }}
+				whileHover={{ scale: 1.25 }}
+				href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
 				<img className="music-link-icon" src={icon} alt="logo" />
-			</a>
+			</motion.a>
 		</div>
 	);
 };
--- src/Pages/Music/Music.jsx
+++ src/Pages/Music/Music.jsx
@@ -5,9 +5,7 @@ import spotifyIcon from "../../assets/svg/iOS/spotify-icon.svg";
 import MyMusic from "../../Components/MyMusic/MyMusic";
 import "./Music.css";
 import featuredImg from "../../assets/images/badsnacks-reference-img.png";
-// [ ] apple and soundcloud logo]
-// [ ] finish completely styling the featured section
-// [ ] complete the connect section
+
 const Music = () => {
 	return (
 		<>
