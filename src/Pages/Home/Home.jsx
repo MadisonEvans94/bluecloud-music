0f76c2f2c1b75608b62fd7c893a6daf3f3bdff07
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    bug fixes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navigation from "../../Components/Navigation/Navigation";
-import ProductPage from "../Store/Store";
-import Contact from "../Contact/Contact";
-import Checkout from "../Checkout/Checkout";
-import Music from "../../Pages/Music/Music";
 
 const backgroundColor = "#2ca7ff";
 
