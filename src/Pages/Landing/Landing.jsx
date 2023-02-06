edc21d19723543a8dc271fa9b7552ab1a8711de9
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    added title

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import Clouds from "../../Components/Clouds/Clouds";
+import styled from "styled-components";
 
 const Landing = () => {
 	return (
 		<>
-			<div>landing page</div>
+			<div
+				style={{
+					position: "absolute",
+					left: "50%",
+					bottom: "30%",
+					transform: "translate(-50%, -50%)",
+				}}>
+				<h1 style={{ color: "white", fontSize: "5rem" }}>blue cloud</h1>
+			</div>
 			<Clouds />
 		</>
 	);
