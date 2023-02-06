524d05ae12330740e7c84e1a35c2af28ed492565
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    style reformatting

--- src/Components/Card/ProductCard.jsx
+++ src/Components/Card/ProductCard.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import titleFont from "../../assets/Lafonsa Font/Lafonsa.ttf";
-import AddToBasketButton from "../Utility/AddToBasketButton";
 import styled from "styled-components";
+import "./ProductCard.css";
 
-const ProductCardContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	justify-self: center;
-	max-width: 65rem;
-	min-width: 50rem
-
-	margin-inline: auto;
-`;
-/**
- * A styled component for the ImageContainer
- */
 const ImageContainer = styled.div`
 	background-image: url(${(props) => props.backgroundImg});
 	background-size: cover;
@@ -29,9 +17,6 @@ const ImageContainer = styled.div`
 	margin: 20px;
 `;
 
-/**
- * A styled component for the title
- */
 const Title = styled.h2`
 	font-family: "Title Font", sans-serif;
 
@@ -47,63 +32,15 @@ const Title = styled.h2`
 	margin-bottom: 10px;
 `;
 
-/**
- * A styled component for the description
- */
-const Description = styled.p`
-	color: white;
-	font-size: 16px;
-	margin-bottom: 10px;
-`;
-
-/**
- * A styled component for the price
- */
-const Price = styled.p`
-	color: white;
-	font-size: 18px;
-	font-weight: bold;
-`;
-
-/**
- * A card component that displays a background image, title, description, and price
- *
- * @param {string} backgroundImg - The URL of the background image
- * @param {string} title - The title to be displayed
- * @param {string} description - The description to be displayed
- * @param {string} price - The price to be displayed
- */
 const ProductCard = ({ backgroundImg, title, description, price }) => (
 	<>
-		<ProductCardContainer>
-			<div
-				style={{
-					margin: "1rem 1rem 1rem 1rem",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "space-between",
-					alignItems: "space-between",
-					width: "fit-content",
-					maxWidth: "40rem",
-				}}>
+		<div className="product-card">
+			<div className="product-card-text">
 				<Title>{title}</Title>
-				<hr
-					style={{
-						color: "white",
-						border: "none",
-						backgroundColor: "white",
-						width: "100%",
-						height: "1px",
-					}}
-				/>
-				<Description>{description}</Description>
-				<Price>{price}</Price>
-
-				{/* TODO create a separate component for adding items to basket. "ItemCounter" */}
-				<AddToBasketButton />
+				<p className="description">{description}</p>
 			</div>
 			<ImageContainer backgroundImg={backgroundImg}></ImageContainer>
-		</ProductCardContainer>
+		</div>
 	</>
 );
 
