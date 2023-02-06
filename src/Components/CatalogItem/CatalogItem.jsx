713e3398d9369e9bbbb607a8072b11ec4ed60959
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    checkpoint

--- src/Components/CatalogItem/CatalogItem.jsx
+++ src/Components/CatalogItem/CatalogItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 const CatalogItem = ({ song, img }) => {
 	return (
 		<>
 			<div className="catalog-item">
-				<div className="song-overlay" />
+				<motion.div
+					className="song-overlay"
+					initial={{ opacity: 0, transition: { duration: 0.1 } }}
+					whileHover={{ opacity: 1, transition: { duration: 0.1 } }}
+				/>
 				<img src={img} alt="" />
 			</div>
 		</>
