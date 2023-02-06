533e7a74f6e97208f16b3332563c786dba029bd4
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    spotify link set up

--- src/Pages/Music/Music.jsx
+++ src/Pages/Music/Music.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import spotifyLogo from "../../assets/svg/iOS/spotify-icon.svg";
 
 const Music = () => {
-	return <div>Music</div>;
+	//[ ] add apple svg and link
+	//[ ] add spotify svg and link
+
+	console.log(spotifyLogo);
+
+	return (
+		<>
+			<div
+				style={{
+					display: "flex",
+					width: "fit-content",
+				}}>
+				<a href="https://open.spotify.com/artist/3W9mqyUptTa6FdiwM9SH11">
+					<img
+						src={spotifyLogo}
+						alt="logo"
+						style={{
+							minWidth: "10rem",
+							margin: "2rem",
+							color: "white",
+						}}
+					/>
+				</a>
+			</div>
+		</>
+	);
 };
 
 export default Music;
