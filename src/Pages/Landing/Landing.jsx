3562203ae5e9c9b3a6acfaf91d36b7206376f0c3
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    removed landing title component

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -3,21 +3,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import Clouds from "../../Components/Clouds/Clouds";
 import "./Landing.css";
-import styled from "styled-components";
-// whileTap={{ scale: 0.9 }}
-
-const LandingTitle = styled.h1`
-	position: absolute;
-
-	color: white; 
-	font-size:5rem; 
-	width: fit-content;  
-	height; 100%;
-	left: 50%; 
-	bottom: 20%; 
-	transform: translate(-50%, -50%); 
-	
-`;
 
 const Landing = () => {
 	const [isClicked, setIsClicked] = useState(false);
@@ -34,9 +19,11 @@ const Landing = () => {
 					zIndex: "-10",
 				}}>
 				<Link to="/home/music">
-					<LandingTitle onClick={() => setIsClicked((prev) => !prev)}>
+					<h1
+						className="landing-title"
+						onClick={() => setIsClicked((prev) => !prev)}>
 						blue cloud
-					</LandingTitle>
+					</h1>
 				</Link>
 
 				<Clouds isClicked={isClicked} />
