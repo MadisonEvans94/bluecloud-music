056c58fa672b2beb86230128bc2a017359fcd0a1
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    animation added to my music section

--- src/Components/MyMusic/MyMusic.jsx
+++ src/Components/MyMusic/MyMusic.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import CatalogItem from "../CatalogItem/CatalogItem";
 import img from "../../assets/images/songImg.png";
-
+import { motion } from "framer-motion";
 const MyMusic = () => {
 	return (
-		<div className="my-music-container">
+		<motion.div
+			className="my-music-container"
+			initial={{ x: 600, opacity: 0 }}
+			animate={{ x: 0, opacity: 1 }}
+			transition={{ type: "spring", stiffness: 100, damping: 20 }}>
 			<h2 className="my-music-header">My Music</h2>
 
 			<div className="music-list-card">
 				<CatalogItem img={img} />
 				<CatalogItem img={img} />
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
