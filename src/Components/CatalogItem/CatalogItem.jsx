f697b07d94b38044a12cafe567c46499f74ae4e6
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    react icons

--- src/Components/CatalogItem/CatalogItem.jsx
+++ src/Components/CatalogItem/CatalogItem.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { FaPlayCircle } from "react-icons/fa";
 const CatalogItem = ({ song, img }) => {
 	return (
 		<>
 			<div className="catalog-item">
-				<motion.div
+				<motion.button
 					className="song-overlay"
 					initial={{ opacity: 0, transition: { duration: 0.1 } }}
-					whileHover={{ opacity: 1, transition: { duration: 0.1 } }}
-				/>
+					whileHover={{
+						opacity: 1,
+						transition: { duration: 0.1 },
+					}}>
+					<FaPlayCircle
+						size="30%"
+						color="white"
+						style={{ cursor: "pointer" }}
+					/>
+				</motion.button>
+
 				<img src={img} alt="" />
 			</div>
 		</>
