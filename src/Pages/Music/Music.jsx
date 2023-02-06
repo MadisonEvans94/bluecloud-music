6644bab64bfef1285bbb3d0a800cd276226ac083
Madison Evans <m.corbinevans@gmail.com>
2023-02-06 01:01:14 -0700

    added new section to music grid with animation

--- src/Pages/Music/Music.jsx
+++ src/Pages/Music/Music.jsx
@@ -5,7 +5,7 @@ import spotifyIcon from "../../assets/svg/iOS/spotify-icon.svg";
 import MyMusic from "../../Components/MyMusic/MyMusic";
 import "./Music.css";
 import featuredImg from "../../assets/images/badsnacks-reference-img.png";
-
+import { motion } from "framer-motion";
 const Music = () => {
 	return (
 		<>
@@ -13,6 +13,13 @@ const Music = () => {
 				<MusicHero icon={spotifyIcon} />
 				<Featured featuredImg={featuredImg} />
 				<MyMusic />
+				<motion.div
+					className="div4"
+					initial={{ y: 600, opacity: 0 }}
+					animate={{ y: 0, opacity: 1 }}
+					transition={{ type: "spring", stiffness: 100, damping: 20 }}>
+					<div className="div4-container">asdf</div>
+				</motion.div>
 			</div>
 		</>
 	);
