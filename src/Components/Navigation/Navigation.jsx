9812ff6b079385ffc21edaa2348153bf81314e90
Madison Evans <m.corbinevans@gmail.com>
2023-03-02 17:35:59 -0700

    hamburger toggles visibility

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaBars as Hamburger } from "react-icons/fa";
+import { useState, useEffect } from "react";
 import "./Navigation.css";
+const asdf = "hamburger";
 const Navigation = () => {
+	const [dropdownVisibility, setDropdownVisibility] = useState("block");
+
+	function handleClick() {
+		setDropdownVisibility((prev) => {
+			if (prev === "none") {
+				return "block";
+			} else {
+				return "none";
+			}
+		});
+		console.log(dropdownVisibility);
+	}
 	return (
 		<>
 			<div className="nav">
 				<div className="nav-container">
 					<Hamburger
 						className="hamburger"
+						style={{ display: dropdownVisibility }}
 						size="100%"
-						style={{ strokeWidth: 1 }}
+						onClick={handleClick}
 					/>
 					<div>
 						<Link className="navlink" to="/home/store">
