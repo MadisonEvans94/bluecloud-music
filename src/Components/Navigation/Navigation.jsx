e09ae81063c996806630720f04087abed5a46f15
Madison Evans <m.corbinevans@gmail.com>
2023-03-02 17:00:39 -0700

    nav style organizing update

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import "./Navigation.css";
 const Navigation = () => {
 	return (
 		<>
 			<div style={{ top: "0", left: "0", width: "100%" }} className="nav">
 				<div className="nav-container">
-					<Link to="/home/store">Store</Link>
-					<Link to="/home/music">Music</Link>
-					<Link to="/home/contact">Contact</Link>
+					<div>
+						<Link className="navlink" to="/home/store">
+							Store
+						</Link>
+					</div>
+					<div>
+						<Link className="navlink" to="/home/music">
+							Music
+						</Link>
+					</div>
+					<div>
+						<Link className="navlink" to="/home/contact">
+							Contact
+						</Link>
+					</div>
 				</div>
 				<hr
 					style={{
@@ -24,19 +35,5 @@ const Navigation = () => {
 		</>
 	);
 };
-
-/**
- * Styled component for the navigation container
- */
-const NavContainer = styled.nav``;
-
-/**
- * Styled component for the navigation links
- */
-const NavLink = styled(Link)`
-	color: #fff;
-	text-decoration: none;
-	margin-right: 1rem;
-`;
 
 export default Navigation;
