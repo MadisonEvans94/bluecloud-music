a1f4e58ef8255ef75598ccf8d70fb8c86f17672d
Madison Evans <m.corbinevans@gmail.com>
2023-03-07 19:57:19 -0700

    standin nav added

--- src/Components/Navigation/ResponsiveNav.jsx
+++ src/Components/Navigation/ResponsiveNav.jsx
@@ -6,7 +6,7 @@ const ResponsiveNav = () => {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
 	return (
-		<nav className="bg-gray-800">
+		<nav className="bg-gray-800 w-full">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center justify-between h-16">
 					<div className="flex items-center">
