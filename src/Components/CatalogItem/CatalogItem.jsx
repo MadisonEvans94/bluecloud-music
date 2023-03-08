11c85af6e71a331694e6a919ec4efdd52780a9fc
Madison Evans <m.corbinevans@gmail.com>
2023-03-08 12:49:58 -0700

    dummy place holder for catalog item

--- src/Components/CatalogItem/CatalogItem.jsx
+++ src/Components/CatalogItem/CatalogItem.jsx
@@ -6,13 +6,8 @@ const CatalogItem = ({ url }) => {
 	const urlColorWrapper = `${url}?color=0064BE`;
 
 	return (
-		<div className="catalog-item">
-			<ReactPlayer
-				url={urlColorWrapper}
-				playing={false}
-				controls={true}
-				width="100%"
-			/>
+		<div className="catalog-item w-10 h-10 bg-white">
+			<ReactPlayer url={null} playing={false} controls={true} width="100%" />
 		</div>
 	);
 };
