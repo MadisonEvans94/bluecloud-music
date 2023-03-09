91d5fc9d006c5302c587dc3f0c3e4b12cea0b278
Madison Evans <m.corbinevans@gmail.com>
2023-03-09 10:51:39 -0700

    trying something with the nav

--- src/Components/Navigation/ResponsiveNav.jsx
+++ src/Components/Navigation/ResponsiveNav.jsx
@@ -10,8 +10,7 @@ const ResponsiveNav = () => {
 		<motion.nav
 			// initial={{ y: -200 }}
 			// animate={{ y: 0 }}
-			className="w-full"
-			style={{ backgroundColor: "#0e2743", borderBottom: "solid #ddd 1px" }}>
+			className="w-full">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 				<div className="flex items-center h-16">
 					<div className="flex items-center justify-between w-full">
