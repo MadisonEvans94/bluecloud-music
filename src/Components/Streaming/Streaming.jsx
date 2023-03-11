04e90504e5bbe8dd21735aa548eb2cbec0a61b45
Madison Evans <m.corbinevans@gmail.com>
2023-03-11 09:08:24 -0700

    motion animation added to streaming component

--- src/Components/Streaming/Streaming.jsx
+++ src/Components/Streaming/Streaming.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import { BsSpotify } from "react-icons/bs";
 import { FaApple } from "react-icons/fa";
 import { FaSoundcloud } from "react-icons/fa";
+import { motion } from "framer-motion";
 const Streaming = () => {
 	return (
-		<div
+		<motion.div
+			initial={{ x: "-20vw", opacity: 0 }}
+			animate={{
+				x: 0,
+				opacity: 1,
+				transition: { type: "spring", stiffness: 40, damping: 10 },
+			}}
+			exit={{
+				x: "-50vw",
+				opacity: 0,
+				transition: {
+					type: "tween",
+					ease: "easeInOut",
+					duration: 0.8,
+				},
+			}}
 			className="streaming
             h-full flex flex-row justify-around items-center
     		">
@@ -17,7 +33,7 @@ const Streaming = () => {
 			<div className="w-full h-full flex flex-row items-center justify-center">
 				<FaSoundcloud size="10em" color="white" />
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
