2e1edea5d53a33fbc981456ab2632e2bb80c906a
Madison Evans <m.corbinevans@gmail.com>
2023-07-29 09:55:17 -0700

    refactoring and creating Figma components

--- src/Components/Figma/FeaturedContentCard.jsx
+++ src/Components/Figma/FeaturedContentCard.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Subheader from "./Subheader";
 import coverImage from "../../assets/im3.png";
 import Paragraph from "./Paragraph";
+
 const FeaturedContentCard = ({ description = "description" }) => {
 	return (
-		<div className="border">
-			<Layout>
-				<Subheader text="Take Your Time" />
-				<img src={coverImage} alt="img" />
-				<Paragraph>{description}</Paragraph>
-			</Layout>
-		</div>
+		<Layout>
+			<Subheader text="Take Your Time" />
+			<img src={coverImage} alt="img" />
+			<Paragraph>{description}</Paragraph>
+		</Layout>
 	);
 };
 
