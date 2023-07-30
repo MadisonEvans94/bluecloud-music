b19767a3391741c55449d8316ea59cacaf3f8559
Madison Evans <m.corbinevans@gmail.com>
2023-07-30 15:47:41 -0700

    refactoring and creating Figma components

--- src/Components/Figma/FeaturedContentCard.jsx
+++ src/Components/Figma/FeaturedContentCard.jsx
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
 			<div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
 				<div
 					className="
-					rounded-xl mx-auto overflow-hidden my-5 flex flex-col items-center
+					rounded-xl mx-auto overflow-hidden my-5 flex flex-col items-center max-w-[500px]
 					"
 				>
 					{children[1]}
