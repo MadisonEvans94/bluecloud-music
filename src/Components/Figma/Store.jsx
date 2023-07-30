b07198861b88d108d1d5edd3563af8329e0c03ba
Madison Evans <m.corbinevans@gmail.com>
2023-07-30 10:45:35 -0700

    refactoring and creating Figma components

--- src/Components/Figma/Store.jsx
+++ src/Components/Figma/Store.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import StoreItem from "./StoreItem";
+import Header from "./Header";
 
+const storeItems = [
+	{
+		itemName: "foo",
+		itemDescription: "bar",
+		imageSrc:
+			"https://plus.unsplash.com/premium_photo-1681433368405-70c0d1205831?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60",
+	},
+	{
+		itemName: "foo",
+		itemDescription: "bar",
+		imageSrc:
+			"https://plus.unsplash.com/premium_photo-1671211326876-82fc00e80590?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+	},
+	{
+		itemName: "foo",
+		itemDescription: "bar",
+		imageSrc:
+			"https://images.unsplash.com/photo-1689850969062-f7e0a6c45a4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1635&q=80",
+	},
+];
 const Store = () => {
 	return (
 		<div>
-			<StoreItem />
+			<Header text="Store" />
+			{storeItems.map((storeItem, i) => {
+				return (
+					<StoreItem
+						key={i}
+						itemName={storeItem.itemName}
+						itemDescription={storeItem.itemDescription}
+						imageSrc={storeItem.imageSrc}
+					/>
+				);
+			})}
 		</div>
 	);
 };
--- src/Components/Figma/StoreItem.jsx
+++ src/Components/Figma/StoreItem.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import Subheader from "./Subheader";
-import Paragraph from "./Paragraph";
 
-const StoreItem = () => {
+import shoppingCart from "../../assets/Figma/icons/shoppingCart.svg";
+
+const StoreItem = ({ itemName = "item", imageSrc = "", itemDescription }) => {
 	return (
-		<div className="cursor-pointer border relative border-info h-[390px] w-[390px] mx-auto rounded-lg overflow-hidden">
-			<div className="bg-primary z-40 w-full h-32 absolute bottom-0 px-5">
-				<h2 className="mt-5 text-info text-subHeadingMobile">
-					Store Item
-				</h2>
-				<p className="text-info">
-					Lorem ipsum dolor sit amet conseur adipisicing elit. Sit
-				</p>
+		<div className="my-8 border relative border-info h-[390px] w-[390px] mx-auto rounded-[32px] overflow-hidden">
+			<div className="bg-primary flex flex-row items-center z-40 w-full h-32 absolute bottom-0 px-3">
+				<div>
+					<h2 className=" text-info text-subHeadingMobile">
+						{itemName}
+					</h2>
+					<p className="text-info">{itemDescription}</p>
+				</div>
+
+				<img
+					className="absolute bottom-4 right-4 cursor-pointer drop-shadow-default hover:drop-shadow-hover active:drop-shadow-active transition"
+					src={shoppingCart}
+					alt=""
+				/>
 			</div>
 			<img
-				className="object-cover h-full object-center "
-				src="https://plus.unsplash.com/premium_photo-1681433368405-70c0d1205831?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
+				className="object-cover h-full w-full object-center "
+				src={imageSrc}
 				alt="item-img"
 			/>
 		</div>
