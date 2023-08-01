8978be134e0124695657a9eec1295569425170e2
Madison Evans <m.corbinevans@gmail.com>
2023-08-01 10:05:17 -0700

    working on store section

--- src/Components/Figma/StoreContent.jsx
+++ src/Components/Figma/StoreContent.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-import Header from "./Header";
-import Paragraph from "./Paragraph";
+import Carousel from "./Carousel";
+import StoreItem from "./StoreItem";
+
 // TODO
+// [x] import store item
+// [x] create fake list of store items
+// [x] import carousel
+// [ ] integrate carousel with store items
+// [ ] make it pretty
+
+const storeItems = [
+	<StoreItem
+		itemName="item 1"
+		itemDescription="placeholder for sample pack 1"
+		imageSrc="https://images.unsplash.com/photo-1604871000636-074fa5117945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+	/>,
+	<StoreItem
+		itemName="item 2"
+		itemDescription="placeholder for sample pack 2"
+		imageSrc="https://images.unsplash.com/photo-1618331833071-ce81bd50d300?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1635&q=80"
+	/>,
+	<StoreItem
+		itemName="item 3"
+		itemDescription="placeholder for sample pack 3"
+		imageSrc="https://plus.unsplash.com/premium_photo-1664438942574-e56510dc5ce5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2671&q=80"
+	/>,
+];
+
 const StoreContent = React.forwardRef(({ className }, ref) => {
 	return (
 		<div
@@ -9,10 +34,7 @@ const StoreContent = React.forwardRef(({ className }, ref) => {
 			id="store"
 			className={`h-screen pl-10 pr-24 flex flex-col justify-center my-auto relative snap-center ${className}`}
 		>
-			<Header>Sample Pack</Header>
-			<Paragraph>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-			</Paragraph>
+			<Carousel items={storeItems} />
 		</div>
 	);
 });
