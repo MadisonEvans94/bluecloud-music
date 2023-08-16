af39b72ef168b6b817018a3f0f28c269c495a659
Madison Evans <m.corbinevans@gmail.com>
2023-08-16 10:07:43 -0700

    structure changes to blog content section

--- src/Components/BlogContent.jsx
+++ src/Components/BlogContent.jsx
@@ -11,12 +11,11 @@ const BlogContent = React.forwardRef(({ className, blogs }, ref) => {
 			id="blog"
 			className={`my-auto h-screen bg-cover bg-center snap-center ${className}`}
 		>
-			{/* TODO: update the indexing */}
 			{/* {blogs.map((blog) => (
 				<BlogCard blog={blog} key={blog.blogId} variation={1} />
 			))} */}
 			<div class="grid grid-cols-2 h-full border gap-4 border-black">
-				<div class="col-span-1 row-span-1">
+				<div class="col-span-2 row-span-1">
 					<BlogCard
 						blog={blogs[0]}
 						key={blogs[0].blogId}
