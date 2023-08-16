84b9cb410d680a668a1d4668a99144041b27fb7c
Madison Evans <m.corbinevans@gmail.com>
2023-08-15 20:25:51 -0700

    updating blog section

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -3,10 +3,11 @@ import Subheader from "./Utility/Subheader";
 import Paragraph from "./Utility/Paragraph";
 import { Link } from "react-router-dom";
 
-const BlogCard = ({ blog, version }) => {
+const BlogCard = ({ blog, variation }) => {
+	const variationList = [];
 	return (
 		<Link to={`/blogs/${blog.blogId}`}>
-			<div className="cursor-pointer border rounded-lg pb-5 px-5">
+			<div className="cursor-pointer border rounded-lg pb-5 px-5 w-full h-full">
 				<Subheader text={blog.title} />
 				<Paragraph>{blog.shortDescription}</Paragraph>
 			</div>
--- src/Components/BlogContent.jsx
+++ src/Components/BlogContent.jsx
@@ -9,12 +9,37 @@ const BlogContent = React.forwardRef(({ className, blogs }, ref) => {
 		<div
 			ref={ref}
 			id="blog"
-			className={`pl-10 pr-24 flex flex-col justify-center my-auto relative h-screen bg-cover bg-center snap-center ${className}`}
+			className={`my-auto h-screen bg-cover bg-center snap-center ${className}`}
 		>
 			{/* TODO: update the indexing */}
-			{blogs.map((blog) => (
-				<BlogCard blog={blog} key={blog.blogId} version={1} />
-			))}
+			{/* {blogs.map((blog) => (
+				<BlogCard blog={blog} key={blog.blogId} variation={1} />
+			))} */}
+			<div class="grid grid-cols-2 gap-4 h-full">
+				<div class="col-span-2 row-span-1 p-4">
+					<BlogCard
+						blog={blogs[0]}
+						key={blogs[0].blogId}
+						variation={1}
+					/>
+				</div>
+				<div class="col-span-1 row-span-2 p-4">
+					<BlogCard
+						blog={blogs[1]}
+						key={blogs[1].blogId}
+						variation={1}
+					/>
+				</div>
+				<div class="col-span-1 row-span-2 p-4">
+					<BlogCard
+						blog={blogs[2]}
+						key={blogs[2].blogId}
+						variation={1}
+					/>
+				</div>
+			</div>
+
+			{/* <BlogCard blog={blogs[0]} key={blogs[0].blogId} variation={1} /> */}
 		</div>
 	);
 });
